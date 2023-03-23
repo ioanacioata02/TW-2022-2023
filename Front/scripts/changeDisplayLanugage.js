feae257b2763eee05264b3b8e96da2890504e521
Farcasi Alexandru <farcasialexandrububu@gmail.com>
2023-03-23 23:07:53 +0200

    mainPage

--- Front/scripts/changeDisplayLanugage.js
+++ Front/scripts/changeDisplayLanugage.js
@@ -1,6 +1,6 @@
-let img = document.querySelector('.currentDisplayFlag');
-let language1= document.querySelector('#li1');
-let language2= document.querySelector('#li2');
+let img = document.querySelector('#displayed-image');
+let language2= document.querySelector('#language-eng');
+let language1= document.querySelector('#language-ro');
 let dropDown = document.querySelector('.dropdownItems')
 language1.addEventListener('click', ()=>{img.src='../assets/flag_ro.svg'});
 language2.addEventListener('click', ()=>{img.src='../assets/flag_en.svg'});
