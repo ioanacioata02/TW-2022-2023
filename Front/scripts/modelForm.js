f82f80f36581ec4e981e8be52b9de09fde5fa4ef
ioanacioata02 <ioana.larisa.29@gmail.com>
2023-04-03 20:41:56 +0300

    added model form

---
+++ Front/scripts/modelForm.js
@@ -0,0 +1,15 @@
+let titleEx = "Two Sum";
+var title = document.getElementById("title");
+title.value = titleEx;
+
+
+let descriptionEx="Given an array of integers nums and an integer target, return indices of the \
+two numbers such that they add up to target. \nYou may assume that each input would have exactly \
+one solution, and you may not use the same element twice. Indices of the two numbers such that they \
+add up to target";
+var description = document.getElementById("description");
+description.value = descriptionEx;
+
+let testsEx="{[nums=[2,7,11,15], target=17]([0,3])}.\n{[nums=[4,70,15,20,43], target=35]([2,3])}.\n{[nums=[2,4], target=6]([0,1])}/";
+var tests = document.getElementById("tests");
+tests.value = testsEx;
