6000c817c8f5b80aae7d3739844e8e61cbc88f3a
Farcasi Alexandru <farcasialexandrububu@gmail.com>
2023-04-06 19:34:30 +0300

    Finished problem page, added functionality to login and sign up added some javascript and made a session for loggin

--- Front/scripts/login.js
+++ Front/scripts/login.js
@@ -1,23 +1,30 @@
-
-function loggedIn()
-{
-let element1 = document.querySelector('#log-in');
-let element2=  document.querySelector('#sign-up');
-element2.style.display="none";
-const newItem =  document.createElement('div');
-newItem.id='profile-button';
-newItem.innerHTML='<a href="profile-stats.html">Profile</a>';
-element1.parentNode.replaceChild(newItem, element1);
-}
-
-function loggedOut()
-{
+if(!sessionStorage.getItem("myCat")) {
+    sessionStorage.setItem("myCat", false);
+  }
+  
+  if(sessionStorage.getItem("myCat") === "true") {
+    loggedIn();
+  }
+  
+  function loggedIn() {
+    let element1 = document.querySelector('#log-in');
+    let element2 = document.querySelector('#sign-up');
+    element2.style.display="none";
+    const newItem = document.createElement('div');
+    newItem.id='profile-button';
+    newItem.innerHTML='<a href="profile-stats.html">Profile</a>';
+    element1.parentNode.replaceChild(newItem, element1);
+    sessionStorage.setItem("myCat", true);
+  }
+  
+  function loggedOut() {
     let element1 = document.querySelector('#profile-button');
-    let element2=  document.querySelector('#sign-up');
+    let element2 = document.querySelector('#sign-up');
     element2.style.display="block";
-    const newItem =  document.createElement('div');
+    const newItem = document.createElement('div');
     newItem.id='profile-button';
     newItem.innerHTML = '<a class="menu-anchor" href="login.html" id="log-in">Login</a>';
-
     element1.parentNode.replaceChild(newItem, element1);
-}
+    sessionStorage.setItem("myCat", false);
+  }
+  
