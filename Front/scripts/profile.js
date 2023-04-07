2b38093ab7ee0125f2c5e4cd694efd915f181763
ioanacioata02 <ioana.larisa.29@gmail.com>
2023-04-07 18:40:16 +0300

    linked profile to the other pages

--- Front/scripts/profile.js
+++ Front/scripts/profile.js
@@ -1,10 +1,18 @@
 let role = document.getElementById("rol").innerText.toLocaleLowerCase();
 let proposeProblem = document.getElementById("propune-problema");
+let myClass = document.getElementById("link-to-class");
+let word = document.getElementById("singular-or-plural");
 
 proposeProblem.classList.add("hidden");
 
 if (role === "teacher") {
     proposeProblem.classList.remove("hidden");
+    myClass.setAttribute("href", "myClasses_teacher.html");
+    word.innerHTML="classes";
+}
+else{
+    myClass.setAttribute("href", "myClass_student.html");
+    word.innerHTML="class";
 }
 
 let logout = document.getElementById("logout");
