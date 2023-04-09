e4acafcb1c19f5d06d7cb3a6905ef928e2bc5eca
Farcasi Alexandru <farcasialexandrububu@gmail.com>
2023-04-10 00:13:58 +0300

    Am validat toate fisierele

--- validator.py
+++ validator.py
@@ -1,30 +1,54 @@
 import os
+import sys
 import cssutils
-from html5validator import HTML5Validator
+import tidylib
 
-# Set the directories to validate
-html_directory = "F:\Reps\clone3\TW-2022-2023\Front\source"
-css_directory = "F:\Reps\clone3\TW-2022-2023\Front\styles"
+def validate_html(html_file_path):
+    with open(html_file_path, 'r') as f:
+        html = f.read()
 
-# Get a list of all HTML and CSS files in their respective directories
-html_files = [filename for filename in os.listdir(html_directory) if filename.endswith('.html')]
-css_files = [filename for filename in os.listdir(css_directory) if filename.endswith('.css')]
+    options = {
+        'output-xhtml': 1,
+        'show-errors': 1,
+        'force-output': 1,
+        'wrap': 0
+    }
 
-# Validate each HTML file
-with open('log.txt', 'w') as log_file:
+    result = tidylib.tidy_document(html, options=options)
+    errors = result[1]
+
+    if errors:
+        print(f'Errors found in {html_file_path}:')
+        for error in errors:
+            print(f'Line {error["line"]}, column {error["column"]}: {error["message"]}')
+        return False
+    else:
+        print(f'{html_file_path} is valid HTML.')
+        return True
+
+
+def validate_css(css_file_path):
+    with open(css_file_path, 'r') as f:
+        css = f.read()
+
+    try:
+        cssutils.parseString(css)
+        print(f'{css_file_path} is valid CSS.')
+        return True
+    except Exception as e:
+        print(f'Errors found in {css_file_path}: {e}')
+        return False
+
+
+if __name__ == '__main__':
+    html_dir = '/path/to/html/directory'
+    css_dir = '/path/to/css/directory'
+
+    html_files = [os.path.join(html_dir, f) for f in os.listdir(html_dir) if f.endswith('.html')]
+    css_files = [os.path.join(css_dir, f) for f in os.listdir(css_dir) if f.endswith('.css')]
+
     for html_file in html_files:
-            validator = html5validator()
-            validator.validate_file(os.path.join(html_directory, html_file))
-            log_file.write(f"{html_file} is valid HTML.\n")
+        validate_html(html_file)
 
-
-    # Validate each CSS file
     for css_file in css_files:
-        try:
-            with open(os.path.join(css_directory, css_file), 'rb') as f:
-                cssutils.parseString(f.read())
-            log_file.write(f"{css_file} is valid CSS.\n")
-        except Exception as error:
-            log_file.write(f"{css_file} is not valid CSS: {error}\n")
-
-print("Validation complete. Results written to log.txt")
+        validate_css(css_file)
