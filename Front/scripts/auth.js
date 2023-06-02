e72e53c996588d737c930b3d879c31229e44554f
sanduraluca <sanduraluca0808@gmail.com>
2023-06-02 15:03:39 +0300

    login,signUp updated

--- Front/scripts/auth.js
+++ Front/scripts/auth.js
@@ -1,114 +1,54 @@
-<<<<<<< Updated upstream
-class Auth {
-	constructor() {
-        document.querySelector("body").style.display = "none";
-		const auth = localStorage.getItem("auth");
-		this.validateAuth(auth);
-	}
 
-	validateAuth(auth) {
-		if (auth == 1) {
-            let element1 = document.querySelector('#log-in');
-            let element2 = document.querySelector('#sign-up');
-            element2.style.display="none";
-            //element1.style.display="none";
-            const newItem = document.createElement('div');
-            newItem.id='profile-button';
-            newItem.innerHTML='<a href="profile-stats.html">Profile</a>';
-            element1.parentNode.replaceChild(newItem, element1);
-            sessionStorage.setItem("myCat", true);
-            document.querySelector("body").style.display = "block";
-		}
-	}
 
-	logOut() {
-		localStorage.removeItem("auth");
-		window.location.replace("/");
-	}
-}
-=======
-class Login {
-	constructor(form) {
-		this.form = form;
-		this.validateOnSubmit();
-	}
-
-	validateOnSubmit() {
-		this.form.addEventListener("submit", (e) => {
-			e.preventDefault();
-			const emailField = this.form.querySelector("input[name=email]");
-			const passwordField = this.form.querySelector("input[name=psw]");
-
-			if (this.validateFields(emailField, passwordField)) {
-				// Make an AJAX request to your PHP login API endpoint
-				const formData = new FormData(this.form);
-				const xhr = new XMLHttpRequest();
-				xhr.open("POST", "http://localhost/authentication.php", true);
-				xhr.onload = function () {
-					if (xhr.status === 200) {
-						const response = JSON.parse(xhr.responseText);
-						if (response.success) {
-							localStorage.setItem("myCat", 1);
-							const previousPage = document.referrer || "index.html";
-							window.location.href = previousPage;
-						} else {
-							const errorContainer = document.createElement("div");
-							errorContainer.classList.add("error-message");
-							errorContainer.textContent = response.message;
-							emailField.parentElement.appendChild(errorContainer);
-						}
-					} else {
-						console.log("An error occurred during the login process.");
-					}
-				};
-				xhr.send(formData);
-			}
-		});
-	}
-
-	validateFields(...fields) {
-		let isValid = true;
-		fields.forEach((field) => {
-			if (field.value.trim() === "") {
-				this.setStatus(
-					field,
-					`${field.previousElementSibling.textContent} cannot be blank`,
-					"error"
-				);
-				isValid = false;
-			} else {
-				this.setStatus(field, null, "success");
-			}
-		});
-		return isValid;
-	}
-
-	setStatus(field, message, status) {
-		const errorContainer = field.parentElement.querySelector(".error-message");
-
-		if (status === "success") {
-			if (errorContainer) {
-				errorContainer.remove();
-			}
-			field.classList.remove("input-error");
-		}
-
-		if (status === "error") {
-			if (!errorContainer) {
-				const errorContainer = document.createElement("div");
-				errorContainer.classList.add("error-message");
-				field.parentElement.appendChild(errorContainer);
-			}
-			errorContainer.textContent = message;
-			field.classList.add("input-error");
-		}
-	}
+function parseJwt(token) {
+  if (!token){ 
+    return;
+  }
+  const base64 = token.split('.')[1]; // extracting payload
+  return JSON.parse(window.atob(base64));
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-	const form = document.querySelector(".login-form");
-	if (form) {
-		const validator = new Login(form);
-	}
+//login
+loginForm = document.getElementById("log-in");
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(loginForm);
+  const loginData = {
+    "email": formData.get('email'),
+    "password": formData.get('password')
+  };
+  try {
+    const response = await fetch('http://localhost/authentication', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginData)
+    });
+    if (!response.ok) {
+      if (response.status === 401) {
+        let err = document.querySelector('form.login .error');
+        err.classList.add('show');
+      }
+      return;
+    }
+    const data = await response.json();
+    const token = data.token;
+    console.log(token);
+    localStorage.setItem('token', token);
+    if(token != null){
+      let role = parseJwt(token).role.toLowerCase();
+      if(role == "user")
+        window.location.href = 'home.html';
+      else
+        window.location.href = 'admin.html';
+    }
+    else{
+      const err = document.querySelector('form.login .error');
+      err.classList.add('show');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  }
 });
->>>>>>> Stashed changes
+
