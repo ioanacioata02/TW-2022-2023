1c8b7e758840c23e5be0dba46ab2a4b53f373d77
sanduraluca <sanduraluca0808@gmail.com>
2023-06-18 00:23:14 +0300

    update homework.js

--- Front/scripts/homeworks.js
+++ Front/scripts/homeworks.js
@@ -46,7 +46,8 @@ async function fetchHomeworks() {
       const myButton = document.querySelector('.myButton');
       const homeworkButton = myButton.querySelector('.homeworkButton');
       console.log(status);
-      if (status === 1) {
+      console.log("ma actualizez");
+      if (status === '1') {
         myButton.style.display = 'block';
         homeworkButton.style.display = 'block';
       } else {
