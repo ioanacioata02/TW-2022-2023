518cb01a34ad1bb29b032a0cc4c1a99b112b67cd
Farcasi Alexandru <farcasialexandrububu@gmail.com>
2023-06-18 11:33:23 +0300

    Bug x3;

--- Front/scripts/problemLogic.js
+++ Front/scripts/problemLogic.js
@@ -9,7 +9,7 @@ function popualteData()
    
     document.getElementById("title-text").textContent=urlParams.get("id")+" ."+urlParams.get("name");
     document.getElementById("description-text").textContent=urlParams.get('description');
-    document.getElementById("rating-text").textContent=urlParams.get("acceptance");
+    //document.getElementById("rating-text").textContent=urlParams.get("acceptance");
     document.getElementById("difficulty-text").textContent=urlParams.get("difficulty");
     //console.log(text)
 }
